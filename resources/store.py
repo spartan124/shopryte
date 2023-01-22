0085e3fada8c8fdcc4c2119fb1e68785019bee95
spartan124 <ojonuba93@gmail.com>
2023-01-22 23:32:57 +0100

    Made some corrections to the store resource

--- resources/store.py
+++ resources/store.py
@@ -1,35 +1,40 @@
-import uuid
-from flask import jsonify, request
+from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import db
 from model import StoreModel
 from schemas import StoreSchema
 from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import jwt_required
 
 blp = Blueprint("store", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @jwt_required()
     @blp.response(200, StoreSchema)
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
         return store
     
+    @jwt_required()
     def delete(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
+        # store = StoreModel.query.filter_by(id=store_id).first()  #does not throw an error if store_id does not exist
         db.session.delete(store)
         db.session.commit()
         return {"message":"Store Deleted."}
         
 @blp.route("/store")
 class StoreList(MethodView):
+    @jwt_required()
     @blp.response(200, StoreSchema(many=True))
     def get(self):
         return StoreModel.query.all()
-
+    
+    @jwt_required()
     @blp.arguments(StoreSchema)
-    @blp.response(200, StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
         store = StoreModel(**store_data)
 
@@ -37,6 +42,6 @@ class StoreList(MethodView):
             db.session.add(store)
             db.session.commit()
         except SQLAlchemyError:
-            abort(500, message="An error occured while creating a store.")
+            abort(500, message="An error occured while creating the store.")
 
         return store, 201
