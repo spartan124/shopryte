2eaf7a968349d145ecad20048a7d5ce245162c4a
spartan124 <ojonuba93@gmail.com>
2023-01-22 23:28:23 +0100

    Made some corrections to the items resource

--- resources/item.py
+++ resources/item.py
@@ -6,16 +6,17 @@ from db import db
 from model import ItemModel
 from schemas import ItemSchema, UpdateItemSchema
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-
+from flask_jwt_extended import jwt_required
 blp = Blueprint("item", __name__, description="Operations on Items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
-
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -23,6 +24,7 @@ class Item(MethodView):
 
         return {"message":"Item Deleted"}
     
+    @jwt_required()
     @blp.arguments(UpdateItemSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
@@ -30,8 +32,10 @@ class Item(MethodView):
         if item:
             item.price = item_data["price"]
             item.name = item_data["name"]
+            item.store_id = item_data["store_id"]
         else:
             item = ItemModel(id=item_id, **item_data)
+        
         db.session.add(item)
         db.session.commit()
 
@@ -40,18 +44,21 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
 
+    @jwt_required()
     @blp.arguments(ItemSchema)
-    @blp.response(200, ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
         item = ItemModel(**item_data)
 
         try:
             db.session.add(item)
             db.session.commit()
+        
         except IntegrityError:
             abort(400, message="An Item with that name already exists.")
         except SQLAlchemyError:
