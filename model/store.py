b98d7a784b3e71cb60b19f272ec67c124f6ac0e4
spartan124 <ojonuba93@gmail.com>
2023-01-23 00:16:13 +0100

    Updated item and store  models

--- model/store.py
+++ model/store.py
@@ -4,4 +4,4 @@ class StoreModel(db.Model):
     __tablename__ = "stores"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    item =db.relationship("ItemModel", back_populates="store")
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
