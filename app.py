c817cbc8163fe859cf1ace52fc4afe5625f6040f
spartan124 <ojonuba93@gmail.com>
2023-01-23 00:19:27 +0100

    updated db and app config

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from db import db
 import model
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
+from resources.user import blp as UserBlueprint
+from datetime import datetime, timedelta
 import os
 
 def create_app(db_url=None):
@@ -19,13 +23,20 @@ def create_app(db_url=None):
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 	db.init_app(app)
+	
 	api = Api(app)
-
+	app.config["JWT_SECRET_KEY"] = "spartandfjhfdsahfd"
+	app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+	app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+	
+	jwt = JWTManager(app)
+	
 	@app.before_first_request
 	def create_tables():
 		db.create_all()
 	api.register_blueprint(StoreBlueprint)
 	api.register_blueprint(ItemBlueprint)
-
+	api.register_blueprint(UserBlueprint)
+	
 	return app
 	
